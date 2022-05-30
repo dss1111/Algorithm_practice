9e482cf79f23b324c73c2c542450f6651d7db6b7
Jaemoon Yoon <37682970+dss1111@users.noreply.github.com>
2022-05-30 20:03:56 +0900

    백준 N과 M (2) 백트래킹

---
+++ baekjoon/15650.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class bj15650 {
+		static int N,M;
+		static StringBuffer sb;
+		public static void main(String[] args) throws NumberFormatException, IOException {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
+			sb = new StringBuffer();
+			select(M,0,"");
+			System.out.println(sb.toString());
+		}
+		public static void select(int count, int big,String s) {
+			if(count == 0) {
+				s = s.substring(1);
+				sb.append(s+"\n");
+				return;
+			}
+			for(int i = big+1;i<=N;i++) {
+				String next = s + " "+ i;
+				select(count-1,i,next);
+			}
+		}
+}
